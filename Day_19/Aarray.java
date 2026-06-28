063788a8dc299c105dcce9e656da21c9ed60e410
Riddhima Gangwar <riddhima.gangwar9@gmail.com>
2026-06-28 19:41:11 +0530

    first commit

---
+++ Day_19/Aarray.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Aarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        // for(int temp:arr)
+        // {
+        //     System.out.print(temp+" ");
+        // }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
